037567ab30ed369e68c0129a453967830316afc1
Maol19ag <55132308+Maol19ag@users.noreply.github.com>
2019-10-18 09:05:27 +0200

    Hjemmeside 18-10-19

--- ProgProject/js/main.js
+++ ProgProject/js/main.js
@@ -25,13 +25,13 @@ jQuery(document).ready(function(){
         }
     });
     $(window).scroll(function(){
-       var top = $(window).scrollTop();
-        if(top>=60){
-            $("header").addClass("secondary");
-        }
-        else 
-            if($("header").hasClass("secondary")){
-                $("header").removeClass("secondary");
-            }
-    });
+    var top = $(window).scrollTop();
+    if(top>=60){
+        $("header").addClass("secondary");
+    }
+    else
+    if($("header").hasClass("secondary")){
+        $("header").removeClass("secondary");
+    }
+});
 });

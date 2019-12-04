9ef7cb0e1a255393d6629b803aed119ffccc36b5
julieskavhaug <54905792+julieskavhaug@users.noreply.github.com>
2019-12-04 14:28:05 +0100

    Update jquery.shop.js

--- ProgProject/KlekkenSite/js/jquery.shop.js
+++ ProgProject/KlekkenSite/js/jquery.shop.js
@@ -3,7 +3,7 @@ function add_to_cart(id) {
 	if (!window.localStorage.getItem(id)) {
 		window.localStorage.setItem(id, 1);
 	} else {
-		window.localStorage.setItem(id, parseInt(1) + parseInt(window.localStorage.getItem(id)));
+		window.localStorage.setItem(id, 1 + parseInt(window.localStorage.getItem(id)));
 	}
 	alert(window.localStorage.getItem(id));
 }
@@ -14,46 +14,71 @@ function add_to_cart_item(){
 }
 
 
-document.getElementById("tent-1").addEventListener("click", add_to_cart_item);
+//document.getElementById("tent-1").addEventListener("click", add_to_cart_item);
 
 
-/*function add() {
 
-	var tableRef = document.getElementsByClassName('shopping-cart').getElementsByTagName('tbody')[0];
-	var newRow = tableRef.insertRow();
-	var newCell = newRow.insertCell();
-	var newText = document.createTextNode('New row');
-	newCell.appendChild(newText);
-	alert("you did it!")
-}
-*/
 
 function clear_cart(){
-	alert("cart cleared")
+window.localStorage.clear();
+	alert("cart cleared");
 }
 
+
+//fjerne fra localstorage
+function removeItemFromCart(){
+localStorage.removeItem(keyName);
+alert("remomved from cart");
+}
 
 
 
 //inspirasjon fra W3schools
-function deleteRow(r) {
+function deleteRow(r){
 	var i = r.parentElement.parentElement.rowIndex;
 	document.getElementById("itemTable").deleteRow(i);
 }
 
 
 
+// Function to clear user from localStorage
+function clearUser() {
+	localStorage.clear();
+	alert('row deleted');
+}
+
+
+
+var items = [{name:'tent-1', price: 1000}, {name:'tent-2', price: 1500}];
+
+
+
+//window.localStorage.setItem(“keys”, keys);
+
+
+for (item in window.localStorage.getItem('items')){
+	if (window.localStorage.getItem(item.name)){
+		alert ('go')}
+}
+
+
+
 // inspirasjon fra W3schools
-function myCreateFunction() {
+function addRow(){
 	var table = document.getElementById("itemTable");
 	var row = table.insertRow(1);
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
 	var cell3 = row.insertCell(2);
 	var cell4 = row.insertCell(3);
-	cell1.innerHTML = "Item";
-	cell2.innerHTML = "Price";
-	cell3.innerHTML = "Quantity";
-	cell4.innerHTML = "<input type=\"button\" value='Remove' onclick='deleteRow(this)'>";
+	cell1.innerHTML = item.name;
+	cell2.innerHTML = item.price;
+	cell3.innerHTML = window.localStorage.getItem(item.name);
+	cell4.innerHTML = "<input type=\"button\" value='Remove' onclick='deleteRow(this), removeItemFromCart()'>";
 }
+
+
+//forloop
+
+
 
